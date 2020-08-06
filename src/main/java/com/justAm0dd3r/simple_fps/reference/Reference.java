9ce1d0876228077d0b59628a201d5eb36ba89ca8
Krxwallo <a@b.c>
2020-08-06 15:45:16 +0200

    Update Version 1.0.1

--- src/main/java/com/justAm0dd3r/simple_fps/reference/Reference.java
+++ src/main/java/com/justAm0dd3r/simple_fps/reference/Reference.java
@@ -5,7 +5,7 @@ package com.justAm0dd3r.simple_fps.reference;
  */
 public class Reference {
     public static final String MOD_ID = "simple_fps";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "SimpleFPS";
     public static final String AUTHOR = "justAm0dd3r";
 }
