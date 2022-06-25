7bf7a6815963ec081b663eabfb7bfa9202304965
Krxwallo <ken.grill@gmx.de>
2022-06-25 12:08:29 +0200

    :arrow_up: migrate to 1.19

--- src/main/java/com/justAm0dd3r/simple_fps/SimpleFPS.java
+++ src/main/java/com/justAm0dd3r/simple_fps/SimpleFPS.java
@@ -1,7 +1,8 @@
 package com.justAm0dd3r.simple_fps;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,6 +20,8 @@ public class SimpleFPS {
         String fps = Minecraft.getInstance().fpsString.split(" ")[0];
 
         // white = 16777215
-        Minecraft.getInstance().font.draw(event.getMatrixStack(), new TranslatableComponent("gui.simple_fps.main", fps), 3, 3, 16777215);
+        Minecraft.getInstance().font.draw(event.getPoseStack(),
+                MutableComponent.create(new TranslatableContents("gui.simple_fps.main", fps)),
+                3, 3, 16777215);
     }
 }
