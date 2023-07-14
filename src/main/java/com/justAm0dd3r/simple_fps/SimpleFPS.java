f10c22ceec2d041598061296e775bc9c22fe7585
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-07-14 21:02:52 +0200

    make it compile; only show fps when debug info is hidden

--- src/main/java/com/justAm0dd3r/simple_fps/SimpleFPS.java
+++ src/main/java/com/justAm0dd3r/simple_fps/SimpleFPS.java
@@ -1,30 +1,26 @@
 package com.justAm0dd3r.simple_fps;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.contents.TranslatableContents;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.Arrays;
+import net.minecraftforge.server.command.TextComponentHelper;
 
 /**
- * Author: justAm0dd3r
+ * Author: Krxwallo
  */
 @Mod("simple_fps")
 @Mod.EventBusSubscriber
 public class SimpleFPS {
     @SubscribeEvent
     public static void onRenderGameOverlay(RenderGuiOverlayEvent.Post event) {
-        if (!event.getOverlay().equals(RenderGuiOverlayEvent)) return;
+        if (Minecraft.getInstance().options.renderDebug) return;
 
-        String fps = Minecraft.getInstance().fpsString.split(" ")[0];
+        int fps = Minecraft.getInstance().getFps();
+        Component fpsComponent = TextComponentHelper.createComponentTranslation(Minecraft.getInstance().player, "gui.simple_fps.main", fps);
 
         // white = 16777215
-        Minecraft.getInstance().font.drawInBatch(event.getGuiGraphics(),
-                MutableComponent.create(new TranslatableContents("gui.simple_fps.main", fps, new Object[]{})),
-                3, 3, 16777215);
+        event.getGuiGraphics().drawString(Minecraft.getInstance().font, fpsComponent, 3, 3, 16777215);
     }
 }
